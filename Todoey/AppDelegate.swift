2ec1caed560462585dc176cd977990079e359e85
Matheus Evers <matheuseversrfernandes@gmail.com>
2019-06-18 13:37:48 -0300

    Finished Realm

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreData
 import RealmSwift
 
 @UIApplicationMain
@@ -19,43 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         do{
-            let realm = try Realm()
+            _ = try Realm()
         } catch {
             print("Error initialising realm")
         }
         
         return true
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        self.saveContext()
-    }
-    
-    lazy var persistentContainer : NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "DataModel")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    // MARK - Core Data Saving support
-    
-    func saveContext(){
-        let context = persistentContainer.viewContext
-        if context.hasChanges{
-            do{
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-
-
 }
 
